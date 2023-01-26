abaa52c1d1dcb967a2248939df60a611ee7378b3
GromannJulian <julian.gromann96@googlemail.com>
2023-01-26 18:30:02 +0100

    new

---
+++ src/main/java/de/nrw/hagen/fp1589/FPController.java
@@ -0,0 +1,33 @@
+package de.nrw.hagen.fp1589;
+
+public class FPController {
+    public static void main(String[] args) {
+        String filename = "";
+        int requestNumber = 0;
+        if (args.length < 4) {
+            System.out.println("fehlerhafte Anzahl an Argumenten");
+        }
+
+        switch (args[0]) {
+            //Filename
+            case "-f" :
+                filename = args[1];
+                //Input request (Anfrage)
+            case "-i":
+                requestNumber = Integer.parseInt(args[1]);
+        }
+
+        switch (args[2]) {
+            //Filename
+            case "-f" :
+                filename = args[3];
+                //Input request (Anfrage)
+            case "-i":
+                requestNumber = Integer.parseInt(args[3]);
+        }
+
+
+
+
+    }
+}
